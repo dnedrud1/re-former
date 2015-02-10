5c76349c62dcdf1bc7635449840c7e450b43279c
dnedrud1 <dnedrud1@gmail.com>
2015-02-10 22:59:35 +0000

    Went further

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
   
   def create
+    @user = User.new(whitelisted_user_params)
+    if @user.save
+      redirect_to "https://re-former-dnedrud1.c9.io/"
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+  def whitelisted_user_params
+    params.require(:user).permit(:username,:email,:password)
   end
   
 end
